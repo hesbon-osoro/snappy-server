d37ce5608b431ca7db8d07a6397b321703aae5dd
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-08-28 23:28:55 +0300

    url bug fix

--- routes/auth.js
+++ routes/auth.js
@@ -11,7 +11,7 @@ const router = require('express').Router();
 router.post('/login', login);
 router.post('/register', register);
 router.get('/allusers/:id', getAllUsers);
-router.post('/setavatar:id', setAvatar);
+router.post('/setavatar/:id', setAvatar);
 router.get('/logout/:id', logout);
 
 module.exports = router;
